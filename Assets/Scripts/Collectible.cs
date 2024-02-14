7c5a94d730c8d95236829a8a481b7757cea8eb79
JMUCF <ju398578@ucf.edu>
2024-02-14 17:09:43 -0500

    Added collectible item & UI stuff
    Item can be collected and pops up a message
    When enemy and player collide the scene is frozen and a death message pops up

---
+++ Assets/Scripts/Collectible.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+
+    public GameObject itemCollected;
+    public bool itemCollectedBool = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Item collected!");
+            itemCollected.SetActive(true);
+            itemCollectedBool = true;
+            //make coroutine to stop text after 2 seconds
+            //play a sound effect here
+            Destroy(this.gameObject); //destroy the collectible
+            //update gameManager to know item was picked up
+        }
+    }
+}
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,6 +6,8 @@ public class Enemy : MonoBehaviour
 {
     public Transform target;
     public int speed = 5;
+    public GameObject deadText;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,5 +19,17 @@ public class Enemy : MonoBehaviour
     {
         var step =  speed * Time.deltaTime;
         transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("You were caught!");
+            //play a sound effect here
+            //bring up UI to go back to menu
+            deadText.SetActive(true);
+            Time.timeScale = 0; //pause the game
+        }
     }
 }
