acd3e8ade55671ef76ac34478352286c2ea5709e
JMUCF <ju398578@ucf.edu>
2024-03-03 14:00:06 -0500

    Added exit trigger
    Detects when key is picked up or not. Just need to add a scene switch to end screen

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,17 +4,26 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-
-
-    void OnTriggerEnter(Collider collision) //check if player reached exit #### ADD CONDITION CHECK FOR KEY PICKED UP
+    void OnTriggerEnter(Collider other)
     {
-        if (collision.gameObject.tag == "Player")
+        if (other.gameObject.CompareTag("Player"))
         {
-            Debug.Log("You escaped the prison!");
-            //play a sound effect here
-            //bring up UI to go back to menu
-            //deadText.SetActive(true);
-            Time.timeScale = 0; //pause the game
+            // Check if the player has a pickup object component
+            PickupObject pickupObject = other.gameObject.GetComponent<PickupObject>();
+            if (pickupObject != null)
+            {
+                // Check if the key amount is greater than zero
+                if (pickupObject.keyAmount > 0)
+                {
+                    Debug.Log("You escaped the prison! You win!");
+                    // Additional actions you want to take upon winning
+                }
+                else
+                {
+                    Debug.Log("You don't have enough keys to escape!");
+                    // Additional actions you want to take if the player doesn't have enough keys
+                }
+            }
         }
     }
 }
